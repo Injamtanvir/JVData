cce508398d321cf1e6d930faf6594d3a435ce577
INJAM UL HAQUE <injamtanvir@gmail.com>
2025-04-13 13:00:31 +0600

    v4

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,26 @@
+// import React from 'react';
+// import Navbar from './components/Navbar';
+// import DataForm from './components/DataForm';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar />
+//       <div className="container py-4">
+//         <DataForm />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// client/src/App.js
 import React from 'react';
-import Navbar from './components/Navbar';
 import DataForm from './components/DataForm';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -14,4 +34,3 @@ function App() {
 }
 
 export default App;
-
